02e7c6b5368c6cd17cd0e19b7ac70464993b2b74
Natalie N. da Cunha <natalienobre@gmail.com>
2018-08-04 02:54:40 -0300

    Finished lesson

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
+const boardSize = 40;
+const defaultSize = 16;
+
 const container = document.querySelector('#container');
+container.setAttribute('style', `height: ${boardSize}; width: ${boardSize}`);
 
-const dimension = 16;
+var btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  eraseGrid();
+  dimension = validateInput(prompt("Enter grid size:"));
+  generateGrid(dimension);
+  gridTrail();
+});
 
-var lines, columns;
+function generateGrid(dimension){
+  var lines, columns;
+  var edge = 40/dimension;
 
-for (lines = 0; lines < dimension; lines++){
-  const gridLine = document.createElement('gridLine');
-  gridLine.classList.add('gridLine');
+  for (lines = 0; lines < dimension; lines++){
+    const gridLine = document.createElement('gridLine');
+    gridLine.classList.add('gridLine');
+    gridLine.setAttribute('style', `height: ${edge}em`);
 
-  for (columns = 0; columns < dimension; columns++){
-    const square = document.createElement('square');
-    square.classList.add('square');
-    gridLine.appendChild(square);
+    for (columns = 0; columns < dimension; columns++){
+      const square = document.createElement('square');
+      square.classList.add('square');
+      square.setAttribute('style', `height: ${edge}em; width: ${edge}em`);
+      gridLine.appendChild(square);
+    }
+
+    container.appendChild(gridLine);
   }
-
-  container.appendChild(gridLine);
 }
 
-const squares = document.querySelectorAll('square');
+function gridTrail(){
+  const squares = document.querySelectorAll('square');
 
-squares.forEach((square) => {
-  square.addEventListener('mouseover', (e) => {
-    e.target.style.background = 'black';
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', (e) => {
+      e.target.style.background = 'black';
+    });
   });
-});
+}
+
+function eraseGrid(){
+  while (container.hasChildNodes()){
+    container.removeChild(container.firstChild);
+  }
+}
+
+function validateInput(input){
+  if (isNaN(input)){
+    alert("Invalid grid size!");
+    return defaultSize;
+  }
+
+  return input;
+}
+
+generateGrid(defaultSize);
+gridTrail();
